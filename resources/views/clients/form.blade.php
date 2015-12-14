1306d6916a58f86396d34eb2b661a3b4165cc7ec
Bill Lovett <bill@ilovett.com>
2015-12-13 21:57:05 -0500

    Style the client create form with bootstrap

--- resources/views/clients/form.blade.php
+++ resources/views/clients/form.blade.php
@@ -2,61 +2,88 @@
 
 @section('page_main')
 
-{!! Form::model($client, ['route' => 'clients::store']) !!}
+<div class="container">
 
-<div class="field">
-    {!! Form::label('name', 'Name') !!}
-    {!! Form::text('name') !!}
+{!! Form::model($client, ['route' => 'clients::store', 'class' => 'form-horizontal']) !!}
+
+<div class="form-group">
+    {!! Form::label('name', 'Name', ['class' => 'col-sm-2 control-label']) !!}
+    <div class="col-sm-10">
+	{!! Form::text('name', null, ['class' => 'form-control']) !!}
+    </div>
 </div>
 
-<div class="field">
-    {!! Form::checkbox('active', 1, true) !!}
-    {!! Form::label('active', 'This client is active') !!}
+<div class="form-group">
+    <div class="col-sm-offset-2 col-sm-10">
+	<div class="checkbox">
+	    <label>
+		{!! Form::checkbox('active', 1, true) !!}
+		This client is active
+	    </label>
+	</div>
+    </div>
 </div>
 
-<div class="field">
-    {!! Form::label('contact_name', 'Contact Name') !!}
-    {!! Form::text('contact_name') !!}
+<div class="form-group">
+    {!! Form::label('contact_name', 'Contact Name', ['class' => 'col-sm-2 control-label']) !!}
+    <div class="col-sm-10">
+	{!! Form::text('contact_name', null, ['class' => 'form-control']) !!}
+    </div>
 </div>
 
-<div class="field">
-    {!! Form::label('contact_email', 'Contact Email') !!}
-    {!! Form::text('contact_email') !!}
+<div class="form-group">
+    {!! Form::label('contact_email', 'Contact Email', ['class' => 'col-sm-2 control-label']) !!}
+    <div class="col-sm-10">
+	{!! Form::text('contact_email', null, ['class' => 'form-control']) !!}
+    </div>
 </div>
 
-<div class="field">
-    {!! Form::label('address1', 'Mailing Address') !!}
-    {!! Form::text('address1') !!}
+<div class="form-group">
+    {!! Form::label('address1', 'Mailing Address', ['class' => 'col-sm-2 control-label']) !!}
+    <div class="col-sm-10">
+	{!! Form::text('address1', null, ['class' => 'form-control']) !!}
+    </div>
 </div>
 
-<div class="field">
-    {!! Form::label('address2', 'Mailing Address Line 2') !!}
-    {!! Form::text('address2') !!}
+<div class="form-group">
+    <div class="col-sm-offset-2 col-sm-10">
+	{!! Form::text('address2', null, ['class' => 'form-control']) !!}
+    </div>
 </div>
 
-<div class="field">
-    {!! Form::label('city', 'City') !!}
-    {!! Form::text('city') !!}
+<div class="form-group">
+    {!! Form::label('city', 'City', ['class' => 'col-sm-2 control-label']) !!}
+    <div class="col-sm-10">
+	{!! Form::text('city', null, ['class' => 'form-control']) !!}
+    </div>
 </div>
 
-<div class="field">
-    {!! Form::label('locality', 'State') !!}
-    {!! Form::text('locality') !!}
+<div class="form-group">
+    {!! Form::label('locality', 'State', ['class' => 'col-sm-2 control-label']) !!}
+    <div class="col-sm-1">
+	{!! Form::text('locality', null, ['class' => 'form-control', 'maxlength' => 2]) !!}
+    </div>
 </div>
 
-<div class="field">
-    {!! Form::label('postal_code', 'Zip') !!}
-    {!! Form::text('postal_code') !!}
+<div class="form-group">
+    {!! Form::label('postal_code', 'Zip', ['class' => 'col-sm-2 control-label']) !!}
+    <div class="col-sm-2">
+	{!! Form::text('postal_code', null, ['class' => 'form-control', 'maxlength' => 5]) !!}
+    </div>
 </div>
 
-<div class="field">
-    {!! Form::label('phone', 'Phone') !!}
-    {!! Form::text('phone') !!}
+<div class="form-group">
+    {!! Form::label('phone', 'Phone', ['class' => 'col-sm-2 control-label']) !!}
+    <div class="col-sm-10">
+	{!! Form::text('phone', null, ['class' => 'form-control']) !!}
+    </div>
 </div>
 
-<div class="field">
-    {!! Form::submit() !!}
+<div class="form-group">
+    <div class="col-sm-offset-2 col-sm-10">
+	{!! Form::button('submit', ['type' => 'submit', 'class' => 'btn btn-default']) !!}
+    </div>
 </div>
-
 {!! Form::close() !!}
+</div>
 @endsection
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,6 +1,7 @@
 <html>
     <head>
         <title>Toils - {{ $page_title }}</title>
+	<link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}" />
     </head>
     <body>
         <main>
