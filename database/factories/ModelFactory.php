5da2d6ee26bd9814f5d30ade42af5d6f296a9f35
Bill Lovett <bill@ilovett.com>
2015-12-23 21:04:15 -0500

    Seed clients via model factory

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -19,3 +19,19 @@ $factory->define(App\User::class, function (Faker\Generator $faker) {
         'remember_token' => str_random(10),
     ];
 });
+
+$factory->define(App\Client::class, function (Faker\Generator $faker) {
+    return [
+        'user_id' => 1,
+        'active' => $faker->boolean(50),
+        'name' => $faker->company(),
+        'contact_name' => $faker->name(),
+        'contact_email' => $faker->safeEmail(),
+        'address1' => $faker->streetAddress(),
+        'address2' => $faker->secondaryAddress(),
+        'city' => $faker->city(),
+        'locality' => $faker->stateAbbr(),
+        'postal_code' => $faker->postcode(),
+        'phone' => $faker->phoneNumber(),
+    ];
+});
--- database/seeds/ClientsTableSeeder.php
+++ database/seeds/ClientsTableSeeder.php
@@ -11,49 +11,6 @@ class ClientsTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('clients')->insert([
-            'user_id' => 1,
-            'name' => 'Test Client 1',
-            'active' => 1,
-            'contact_name' => 'John Smith',
-            'contact_email' => 'jsmith@example.com',
-            'address1' => '123 Fake Street',
-            'address2' => 'Suite 999',
-            'city' => 'Fakeville',
-            'locality' => 'CA',
-            'postal_code' => '90210',
-            'phone' => '555-555-5555',
-            'created_at' => \Carbon\Carbon::now()->toDateTimeString(),
-            'updated_at' => \Carbon\Carbon::now()->toDateTimeString()
-        ]);
-
-        DB::table('clients')->insert([
-            'user_id' => 1,
-            'name' => 'Test Client 2',
-            'active' => 1,
-            'contact_name' => 'Jane Doe',
-            'contact_email' => 'jdoe@example.com',
-            'address1' => 'Rue de Fakeville',
-            'city' => 'Nowhere',
-            'locality' => 'NV',
-            'postal_code' => '12345',
-            'phone' => '555-867-5309',
-            'created_at' => \Carbon\Carbon::now()->toDateTimeString(),
-            'updated_at' => \Carbon\Carbon::now()->toDateTimeString()
-        ]);
-        DB::table('clients')->insert([
-            'user_id' => 1,
-            'name' => 'Inactive Test Client 1',
-            'active' => 0,
-            'contact_name' => 'Larry Jones',
-            'contact_email' => 'ljones@example.com',
-            'address1' => '1 Whatever Way',
-            'city' => 'Imaginationland',
-            'locality' => 'FL',
-            'postal_code' => '13579',
-            'phone' => '555-333-2222',
-            'created_at' => \Carbon\Carbon::now()->toDateTimeString(),
-            'updated_at' => \Carbon\Carbon::now()->toDateTimeString()
-        ]);
+        factory(App\Client::class, 100)->create();
     }
 }
