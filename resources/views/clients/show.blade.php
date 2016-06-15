6a1a98cbfa1dc92f2503d944f9ccb49ce84ec326
Bill Lovett <bill@ilovett.com>
2016-06-14 20:50:26 -0400

    Reduce repetition on client show template
    Move timestamps to the bottom of the page. Make the left-most column
    Contact rather than Details. Display the contact email and name separately.

--- resources/views/clients/show.blade.php
+++ resources/views/clients/show.blade.php
@@ -1,71 +1,68 @@
 @extends('layouts.master')
 
 @section('page_main')
-<div class="row">
-    <div class="col-sm-4">
-	<h2>Details</h2>
-
-	<div class="well">
-	    <h4>{{ $client->name }}</h4>
-
-	    <p>Created {{ $client->created_at->format('F j, Y \a\t h:i A') }}</p>
-	    <p>Updated {{ $client->updated_at->format('F j, Y \a\t h:i A') }}</p>
-	</div>
-
-	<div class="well">
-	    <h4>Contact</h4>
-	    <address>
-		<a href="mailto:{{ $client->contact_email }}">{{ $client->contact_name }}</a>
-		<div>
-		    {{ $client->address1 }}
-		    {{ $client->address2 }}
-		    {{ $client->city }}, {{ $client->locality }} {{ $client->postal_code }}
-		    <a href="tel://{{ $client->numericPhone() }}">{{ $client->phone }}</a>
-		</div>
-	    </address>
-	</div>
-    </div>
-    <div class="col-sm-8">
-
-	<h2>Projects</h2>
-
-	<div class="row">
-	    <div class="col-sm-6">
-		<div class="well">
-		    <h4>Active</h4>
-		    <ul class="list-unstyled">
-			@if ($client->projects->where('active', true)->count() == 0)
-			    <li>None</li>
-			@endif
-
-			@foreach ($client->projects->where('active', true) as $project)
-			    <li>
-				<a href="{{ route('project.edit', ['project' => $project]) }}">{{ $project->name }}</a>
-			    </li>
-			@endforeach
-		    </ul>
-		</div>
+    <div class="row">
+	<div class="col-sm-4">
+	    <h2>Contact</h2>
+	    <div class="well">
+		<h4>{{ $client->contact_name }}</h4>
+		<address>
+		    <a href="mailto:{{ $client->contact_email }}">{{ $client->contact_email }}</a>
+		    <div>
+			{{ $client->address1 }}
+			{{ $client->address2 }}
+			{{ $client->city }}, {{ $client->locality }} {{ $client->postal_code }}
+			<a href="tel://{{ $client->numericPhone() }}">{{ $client->phone }}</a>
+		    </div>
+		</address>
 	    </div>
-	    <div class="col-sm-6">
-		<div class="well">
-		    <h4>Inactive</h4>
-		    <ul class="list-unstyled">
-			@if ($client->projects->where('active', false)->count() == 0)
-			    <li>None</li>
-			@endif
+	</div>
+	<div class="col-sm-8">
 
-			@foreach ($client->projects->where('active', false) as $project)
-			    <li>
-				<a href="{{ route('project.edit', ['project' => $project]) }}">{{ $project->name }}</a>
-			    </li>
-			@endforeach
-		    </ul>
+	    <h2>Projects</h2>
+
+	    <div class="row">
+		<div class="col-sm-6">
+		    <div class="well">
+			<h4>Active</h4>
+			<ul class="list-unstyled">
+			    @if ($client->projects->where('active', true)->count() == 0)
+				<li>None</li>
+			    @endif
+
+			    @foreach ($client->projects->where('active', true) as $project)
+				<li>
+				    <a href="{{ route('project.edit', ['project' => $project]) }}">{{ $project->name }}</a>
+				</li>
+			    @endforeach
+			</ul>
+		    </div>
+		</div>
+		<div class="col-sm-6">
+		    <div class="well">
+			<h4>Inactive</h4>
+			<ul class="list-unstyled">
+			    @if ($client->projects->where('active', false)->count() == 0)
+				<li>None</li>
+			    @endif
+
+			    @foreach ($client->projects->where('active', false) as $project)
+				<li>
+				    <a href="{{ route('project.edit', ['project' => $project]) }}">{{ $project->name }}</a>
+				</li>
+			    @endforeach
+			</ul>
+		    </div>
 		</div>
 	    </div>
 	</div>
     </div>
-</div>
-
+    <div class="row">
+	<div class="col-sm-12">
+	    Updated {{ $client->updated_at->format('Y-m-d \a\t g:i A') }}.
+	    Created {{ $client->created_at->format('Y-m-d') }}.
+	</div>
+    </div>
 @endsection
 
 @section('nav_primary')
