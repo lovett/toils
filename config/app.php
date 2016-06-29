05d3bfbfac3fa1f2921c16e925c7235788fd2f6e
Bill Lovett <bill@ilovett.com>
2016-06-28 23:16:52 -0400

    Add phing and start a build file for linting
    Also migrates to laravelcollective/html in resposne to deprecation
    notices about illuminates/html.

--- config/app.php
+++ config/app.php
@@ -136,7 +136,6 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-        Illuminate\Html\HtmlServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -153,6 +152,7 @@ return [
          */
         Barryvdh\Debugbar\ServiceProvider::class,
         Propaganistas\LaravelPhone\LaravelPhoneServiceProvider::class,
+        Collective\Html\HtmlServiceProvider::class,
 
     ],
 
@@ -202,12 +202,13 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
-        'Form'      => Illuminate\Html\FormFacade::class,
-        'HTML'      => Illuminate\Html\HtmlFacade::class,
 
         'Debugbar'  => Barryvdh\Debugbar\Facade::class,
         'TimeHelper' => App\Helpers\TimeHelper::class,
         'AddressHelper' => App\Helpers\AddressHelper::class,
+        'Form'      => Collective\Html\FormFacade::class,
+        'HTML'      => Collective\Html\HtmlFacade::class,
+
     ],
 
 ];
