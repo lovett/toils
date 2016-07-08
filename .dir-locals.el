3387268c6dffad7d73ce75fbc5dd317b6a4bcb4b
Bill Lovett <bill@ilovett.com>
2016-07-07 20:15:45 -0400

    Set flycheck paths for phpcs and phpmd via .dir-locals.el

---
+++ .dir-locals.el
@@ -0,0 +1,13 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((php-mode
+  (eval . (progn
+            (set (make-local-variable 'project-root)
+                 (file-name-directory
+                  (let ((d (dir-locals-find-file ".")))
+                    (if (stringp d) d (car d)))))
+            (customize-set-variable 'flycheck-php-phpcs-executable (expand-file-name "vendor/bin/phpcs" project-root))
+            (customize-set-variable 'flycheck-phpcs-standard (expand-file-name "phpcs.xml" project-root))
+            (customize-set-variable 'flycheck-php-phpmd-executable (expand-file-name "vendor/bin/phpmd" project-root))
+            (customize-set-variable 'flycheck-phpmd-rulesets (expand-file-name "phpmd.xml" project-root)))))) 
