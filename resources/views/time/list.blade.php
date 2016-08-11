0988401a8d165a73e0951f8c1d6855cbd32eaeb3
Bill Lovett <bill@ilovett.com>
2016-08-10 22:02:05 -0400

    Rearrange columns on time list

--- resources/views/time/list.blade.php
+++ resources/views/time/list.blade.php
@@ -5,9 +5,10 @@
     <table class="table">
         <thead>
             <tr>
+		<th>ID</th>
                 <th width="200">Date</th>
-                <th width="250">Project</th>
 		<th>Summary</th>
+                <th width="250">Project</th>
 		<th class="text-right">Accuracy</th>
             </tr>
         </thead>
@@ -15,22 +16,27 @@
             @foreach ($times as $time)
             <tr>
                 <td>
+		    {!! link_to_route('time.edit', $time->id, ['id' => $time->id]) !!}
+		</td>
+		<td>
                     {{ $time->start->format('Y-m-d, l') }}
 		    <div class="small">
-			{!! link_to_route('time.edit', $time->start->format('g:i A'), ['id' => $time->id]) !!}
+			{{ $time->start->format('g:i A') }}
 
 			@if ($time->end)
 			    to {{ $time->end->format('g:i A') }}
 			@endif
 		    </div>
                 </td>
-		<td>
-	            {!! link_to_route('project.show', $time->project->name, ['id' => $time->project->id]) !!}
-                </td>
 
 		<td>
 		    {{ str_limit($time->summary, 75) }}
 		</td>
+
+		<td>
+	            {!! link_to_route('project.show', $time->project->name, ['id' => $time->project->id]) !!}
+                </td>
+
 
                 </td>
 		<td class="text-right">
