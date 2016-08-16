0669186edb91fbae0c69e904eaff5a17f4ff6115
Bill Lovett <bill@ilovett.com>
2016-08-15 22:51:22 -0400

    Switch to colon as field/value separator during searches

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -65,17 +65,17 @@ abstract class Controller extends BaseController
      */
     protected function parseSearchQuery(Relation $relation, $query)
     {
-        if (strpos($query, '=') === false) {
+        if (strpos($query, ':') === false) {
             // No fields were specified, so treat the first one as a default.
             $defaultField = current(array_keys($this->searchFields));
 
-            $query = sprintf('%s=%s', $defaultField, $query);
+            $query = sprintf('%s:%s', $defaultField, $query);
         }
 
         // Split by field.
         $fields = array_map(
             function ($field) {
-                return explode('=', $field);
+                return explode(':', $field);
             },
             explode(' ', $query)
         );
@@ -85,6 +85,10 @@ abstract class Controller extends BaseController
             $fields,
             function ($accumulator, $pair) {
                 list($field, $value) = $pair;
+
+                if (is_numeric($value)) {
+                    $value = (int) $value;
+                }
 
                 // Skip unsupported fields.
                 if (array_key_exists($field, $this->searchFields) === false) {
@@ -97,7 +101,7 @@ abstract class Controller extends BaseController
                 }
 
                 // Apply a suffix wildcard to non-wildcard strings.
-                if (is_numeric($value) === false && strpos($value, '*') === false) {
+                if (is_string($value) && strpos($value, '*') === false) {
                     $value .= '*';
                 }
 
