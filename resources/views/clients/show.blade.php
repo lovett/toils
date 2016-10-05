e7d05798b8911607aee5b0a1470e4ab80aab5468
Bill Lovett <bill@ilovett.com>
2016-10-04 22:01:36 -0400

    Whitespace cleanup

--- resources/views/clients/show.blade.php
+++ resources/views/clients/show.blade.php
@@ -2,62 +2,62 @@
 
 @section('page_main')
     <div class="row">
-	<div class="col-sm-4">
-	    <h2>Contact</h2>
-	    <div class="well">
-		@if (empty($model->contactName))
-		    <p>Not specified</p>
-		@else
-		<h4>{{ $model->contactName }}</h4>
-		<address>
-		    <a href="mailto:{{ $model->contactEmail }}">{{ $model->contactEmail }}</a>
-		    <div>
-			{{ AddressHelper::mailingAddress($model) }}
-			{!! AddressHelper::phoneUrl($model->phone) !!}
-		    </div>
-		</address>
-		@endif
-	    </div>
-	</div>
-	<div class="col-sm-8">
+    <div class="col-sm-4">
+        <h2>Contact</h2>
+        <div class="well">
+        @if (empty($model->contactName))
+            <p>Not specified</p>
+        @else
+        <h4>{{ $model->contactName }}</h4>
+        <address>
+            <a href="mailto:{{ $model->contactEmail }}">{{ $model->contactEmail }}</a>
+            <div>
+            {{ AddressHelper::mailingAddress($model) }}
+            {!! AddressHelper::phoneUrl($model->phone) !!}
+            </div>
+        </address>
+        @endif
+        </div>
+    </div>
+    <div class="col-sm-8">
 
-	    <h2>Projects</h2>
+        <h2>Projects</h2>
 
-	    <div class="row">
-		<div class="col-sm-6">
-		    <div class="well">
-			<h4>Active</h4>
-			<ul class="list-unstyled">
-			    @if ($model->projects->where('active', true)->count() == 0)
-				<li>None</li>
-			    @endif
+        <div class="row">
+        <div class="col-sm-6">
+            <div class="well">
+            <h4>Active</h4>
+            <ul class="list-unstyled">
+                @if ($model->projects->where('active', true)->count() == 0)
+                <li>None</li>
+                @endif
 
-			    @foreach ($model->projects->where('active', true) as $project)
-				<li>
-				    <a href="{{ route('project.show', ['project' => $project]) }}">{{ $project->name }}</a>
-				</li>
-			    @endforeach
-			</ul>
-		    </div>
-		</div>
-		<div class="col-sm-6">
-		    <div class="well">
-			<h4>Inactive</h4>
-			<ul class="list-unstyled">
-			    @if ($model->projects->where('active', false)->count() == 0)
-				<li>None</li>
-			    @endif
+                @foreach ($model->projects->where('active', true) as $project)
+                <li>
+                    <a href="{{ route('project.show', ['project' => $project]) }}">{{ $project->name }}</a>
+                </li>
+                @endforeach
+            </ul>
+            </div>
+        </div>
+        <div class="col-sm-6">
+            <div class="well">
+            <h4>Inactive</h4>
+            <ul class="list-unstyled">
+                @if ($model->projects->where('active', false)->count() == 0)
+                <li>None</li>
+                @endif
 
-			    @foreach ($model->projects->where('active', false) as $project)
-				<li>
-				    <a href="{{ route('project.show', ['project' => $project]) }}">{{ $project->name }}</a>
-				</li>
-			    @endforeach
-			</ul>
-		    </div>
-		</div>
-	    </div>
-	</div>
+                @foreach ($model->projects->where('active', false) as $project)
+                <li>
+                    <a href="{{ route('project.show', ['project' => $project]) }}">{{ $project->name }}</a>
+                </li>
+                @endforeach
+            </ul>
+            </div>
+        </div>
+        </div>
+    </div>
     </div>
 
     @include('partials.timestamps-footer', ['record' => $model])
@@ -65,7 +65,7 @@
 
 @section('nav_primary')
     <ul class="list-inline">
-	<li>{!! link_to_route('client.edit', 'Edit', ['id' => $model->id]) !!}</li>
-	<li>{!! link_to_route('project.create', 'New project', ['client' => $model->id]) !!}</li>
+    <li>{!! link_to_route('client.edit', 'Edit', ['id' => $model->id]) !!}</li>
+    <li>{!! link_to_route('project.create', 'New project', ['client' => $model->id]) !!}</li>
     </ul>
 @endsection
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -9,40 +9,40 @@
         <main class="container">
             <header class="primary">
                 <h1>{{ $page_title }}</h1>
-		<div class="row">
+                <div class="row">
                     <nav class="col-sm-6">
                         @yield('nav_primary')
                     </nav>
                     <nav class="col-sm-6 text-right">
-			@section('nav_supplemental')
-			    <ul class="list-inline">
-				<li>{!! LinkHelper::navLink($appSection, 'dashboard', 'Dashboard')  !!}</li>
-				<li>{!! LinkHelper::navLink($appSection, 'time.index', 'Time') !!}</li>
-				<li>{!! LinkHelper::navLink($appSection, 'projects', 'Projects') !!}</li>
-				<li>{!! LinkHelper::navLink($appSection, 'clients', 'Clients') !!}</li>
+                        @section('nav_supplemental')
+                            <ul class="list-inline">
+                                <li>{!! LinkHelper::navLink($appSection, 'dashboard', 'Dashboard')  !!}</li>
+                                <li>{!! LinkHelper::navLink($appSection, 'time.index', 'Time') !!}</li>
+                                <li>{!! LinkHelper::navLink($appSection, 'projects', 'Projects') !!}</li>
+                                <li>{!! LinkHelper::navLink($appSection, 'clients', 'Clients') !!}</li>
                                 <li>{!! LinkHelper::navLink($appSection, 'logout', 'Log out') !!}</li>
-			    </ul>
-			@show
+                            </ul>
+                        @show
                     </nav>
                 </div>
             </header>
 
-	    @if (Session::has('userMessage'))
-	    <div class="alert alert-{{ Session::get('userMessage')[0] }} warning alert-dismissible" role="alert">
-		<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-		{{ Session::get('userMessage')[1] }}
-	    </div>
-	    @endif
+            @if (Session::has('userMessage'))
+                <div class="alert alert-{{ Session::get('userMessage')[0] }} warning alert-dismissible" role="alert">
+                    <button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                    {{ Session::get('userMessage')[1] }}
+                </div>
+            @endif
 
             @yield('page_main')
         </main>
         <script src="{{ asset('js/jquery.min.js') }}"></script>
         <script src="{{ asset('js/select2.min.js') }}"></script>
-	<script src="{{ asset('js/bootstrap.min.js') }}"></script>
+        <script src="{{ asset('js/bootstrap.min.js') }}"></script>
         <script type="text/javascript">
             $('select').select2({'theme': 'bootstrap'});
         </script>
 
-	@yield('page_scripts')
+        @yield('page_scripts')
     </body>
 </html>
