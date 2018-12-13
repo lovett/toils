637167603ad7bbd7fe1994fdf7683e4f2f580e9e
Bill Lovett <bill@ilovett.com>
2018-12-12 20:48:55 -0500

    Prune unused JS libraries to reduce size of app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,16 +1,21 @@
-require('./bootstrap');
+/**
+ * The axios HTTP library automatically handles sending the Laravel
+ * CSRF token as a header based on the value of the "XSRF" token cookie.
+ */
+window.axios = require('axios');
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+
+let token = document.head.querySelector('meta[name="csrf-token"]');
+
+if (token) {
+    window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+} else {
+    console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
+}
 
 window.Vue = require('vue');
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-//Vue.component('example-component', require('./components/ExampleComponent.vue'));
 Vue.component('facet-search', require('./components/FacetSearch.vue'));
-
 Vue.component('autofill', require('./components/Autofill.vue'));
 Vue.component('autofill-hint', require('./components/AutofillHint.vue'));
 Vue.component('pickable', require('./components/Pickable.vue'));
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,16 +1,5 @@
 const mix = require('laravel-mix');
 
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
-
 mix.browserSync({
     notify: false,
     online: true,
@@ -28,5 +17,5 @@ mix.browserSync({
 
 mix.disableNotifications();
 
-mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+mix.js('resources/js/app.js', 'public/js');
+mix.sass('resources/sass/app.scss', 'public/css');
