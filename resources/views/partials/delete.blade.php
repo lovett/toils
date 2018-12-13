c2160e9c55234d915717dd56f8db89e26a9552d9
Bill Lovett <bill@ilovett.com>
2018-12-12 22:09:51 -0500

    Add missing bottom margin to delete button on edit views

--- resources/views/partials/delete.blade.php
+++ resources/views/partials/delete.blade.php
@@ -1,4 +1,4 @@
-<div class="container text-right">
+<div class="container d-flex justify-content-end mb-4">
     {!! Form::model($model, ['route' => [$route, $model->id], 'method' => 'DELETE']) !!}
     {!! Form::button($label, ['type' => 'submit', 'class' => 'btn btn-sm btn-danger']) !!}
     {!! Form::close() !!}
