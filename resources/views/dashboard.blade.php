bef3e0f6b7bbe7c676b0bbe6b9dc58aae8deb065
Bill Lovett <bill@ilovett.com>
2019-01-28 21:32:31 -0500

    Improve layout of dashboard by using card group

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -27,17 +27,19 @@
 
     <div class="row mb-4">
         <div class="col-md-10 offset-md-1">
-            <h2>Projects</h2>
+            <h2>Active Projects</h2>
             <p>{{ TimeHelper::hoursAndMinutes($totalUnbilled) }} of billable time.</p>
-            <div class="grid">
+            <div class="card-deck">
                 @foreach ($activeProjects as $project)
                     <div class="card mb-3">
-                        <div class="card-body">
-                            <h5 class="card-title">{!! link_to_route('project.show', $project->name, ['id' => $project->id]) !!}</h5>
+                        <div class="card-body d-flex flex-column">
+                            <h5 class="card-title flex-fill">{!! link_to_route('project.show', $project->name, ['id' => $project->id]) !!}</h5>
                             <p>{{ TimeHelper::hoursAndMinutes($project->unbilledTime) }}</p>
 
-                            {!! LinkHelper::smallButtonLink('time.create', 'time', ['project' => $project->id]) !!}
-                            {!! LinkHelper::smallButtonLink('invoice.create', 'invoice', ['project' => $project->id]) !!}
+                            <div>
+                            {!! LinkHelper::cardLink('time.create', 'time', ['project' => $project->id]) !!}
+                                {!! LinkHelper::cardLink('invoice.create', 'invoice', ['project' => $project->id]) !!}
+                            </div>
                         </div>
 
                         <div class="card-footer text-muted">
