a959c4ffa147a31e6d158eb1e976598f57091125
Bill Lovett <bill@ilovett.com>
2019-04-09 12:59:34 -0400

    Configure trusted proxy middleware
    Let the application trust all proxies. For this application proxy
    trust isn't an overly pressing issue, especially when served from a
    container where internal IP addresses aren't obvious and are subject
    to change.

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -14,9 +14,9 @@ class TrustProxies extends Middleware
     /**
      * The trusted proxies for this application.
      *
-     * @var array
+     * @var array|string
      */
-    protected $proxies;
+    protected $proxies = '*';
 
     /**
      * The headers that should be used to detect proxies.
