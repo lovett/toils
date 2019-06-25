ff1675991e4d1a66295f452a073694a87588c0f2
Bill Lovett <bill@ilovett.com>
2019-06-25 09:35:34 -0400

    Drop browsersync
    It's no longer a good fit for my dev setup, and not otherwise
    critical. I'd rather have fewer npm dependencies than try to figure
    out a working config that would go unused most of the time.

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,20 +1,5 @@
 const mix = require('laravel-mix');
 
-mix.browserSync({
-    notify: false,
-    online: true,
-    open: false,
-    port: 3010,
-    proxy: {
-        target: 'localhost:8083',
-        reqHeaders: function() {
-            return {
-                host: 'localhost:3010'
-            };
-        }
-    }
-});
-
 mix.disableNotifications();
 
 mix.js('resources/js/app.js', 'public/js');
