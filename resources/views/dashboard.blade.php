54da3323acf6b1fbc9a0e9b467350cc5cc8955d1
Bill Lovett <bill@ilovett.com>
2019-08-19 17:08:12 -0400

    Improve dashboard display on mid-size viewports
    Bootstrap's card-deck class resulted in content getting
    squeezed. Switch to card-group and grid-based determination of
    items-per-row.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -18,15 +18,17 @@
         <div class="col-md-10 offset-md-1">
             <h2>Active Projects</h2>
             <p>{{ TimeHelper::hoursAndMinutes($totalUnbilled) }} of billable time.</p>
-            <div class="card-deck">
+
+            <div class="row">
                 @foreach ($activeProjects as $project)
+                <div class="col-md-6 col-lg-4 card-group">
                     <div class="card mb-3">
                         <div class="card-body d-flex flex-column">
                             <h5 class="card-title flex-fill">{!! link_to_route('project.show', $project->name, ['id' => $project->id]) !!}</h5>
                             <p>{{ TimeHelper::hoursAndMinutes($project->unbilledTime) }}</p>
 
                             <div>
-                            {!! LinkHelper::cardLink('time.create', 'time', ['project' => $project->id]) !!}
+                                {!! LinkHelper::cardLink('time.create', 'time', ['project' => $project->id]) !!}
                                 {!! LinkHelper::cardLink('invoice.create', 'invoice', ['project' => $project->id]) !!}
                             </div>
                         </div>
@@ -35,9 +37,9 @@
                             active {{ TimeHelper::daysAgo($project->updated_at) }}
                         </div>
                     </div>
+                </div>
                 @endforeach
             </div>
-
         </div>
     </div>
 </div>
