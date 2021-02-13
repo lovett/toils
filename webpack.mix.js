3ade37e3cf6d5fa6ad5400cbb033a3aba1ce0aef
Bill Lovett <bill@ilovett.com>
2021-02-12 19:49:30 -0500

    Handle Vue components through mix and resolve webpack troubles.
    Update script arguments to get webpack working again. Put Mix in
    charge of component compilation for the sake of a more standard setup,
    since the previous standalone approach is apparently no longer the
    easy workaround it once was.

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,5 +2,5 @@ const mix = require('laravel-mix');
 
 mix.disableNotifications();
 
-mix.js('resources/js/app.js', 'public/js').version();
+mix.js('resources/js/app.js', 'public/js').vue().version();
 mix.sass('resources/sass/app.scss', 'public/css').version();
