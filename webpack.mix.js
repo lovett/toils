077cda5c0690789f341cb1a8e99512b110bf6aeb
Bill Lovett <bill@ilovett.com>
2021-05-04 19:52:45 -0400

    Restore missing Vue component styles.
    A recent update to Laravel Mix resulted in Vue components rendering
    without styling. Revising mix configuration to use extractStyles
    parameter resolved the problem.

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,5 +2,5 @@ const mix = require('laravel-mix');
 
 mix.disableNotifications();
 
-mix.js('resources/js/app.js', 'public/js').vue().version();
+mix.js('resources/js/app.js', 'public/js').vue({ extractStyles: true}).version();
 mix.sass('resources/sass/app.scss', 'public/css').version();
