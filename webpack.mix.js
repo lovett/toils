617d38a9e751a1ae4ec5a3d741bd4710613fb0f9
Bill Lovett <bill@ilovett.com>
2021-09-17 11:13:35 -0400

    Prevent warning about unmet dependency on prettier.
    Adjust mix's webpack config so that the default use of prettier in
    development is turned off and webpack stops emitting warnings about an
    unmet dependency. This avoids having to install prettier, which is
    otherwise not of interest.

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,5 +2,29 @@ const mix = require('laravel-mix');
 
 mix.disableNotifications();
 
-mix.js('resources/js/app.js', 'public/js').vue({ extractStyles: true}).version();
+mix.js('resources/js/app.js', 'public/js').vue({ extractStyles: true }).version();
 mix.sass('resources/sass/app.scss', 'public/css').version();
+
+// Prevent a warning from vue-loader about prettier dependency.
+//
+// The default configuration of vue-loader uses prettier in
+// development mode. If that package is not installed, webpack will
+// warn about an unmet dependency during development builds.
+//
+// What prettier does isn't especially useful for this application,
+// and having an additional dependency is not desirable.
+//
+// Disabling prettier involves resetting test and loader portions of
+// the loader rule, which seems to work fine but was mostly just a
+// guesss.
+mix.webpackConfig({
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: { prettify: false }
+            }
+        ]
+    }
+});
